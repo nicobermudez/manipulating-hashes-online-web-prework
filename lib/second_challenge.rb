970c4acf234efd4c22f7501f6a91deeedbe8ed12
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-22 22:46:34 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -6,8 +6,12 @@ def second_challenge
    grains: ["rice", "pasta"]
   }
 
+  array = []
   #code your solution here!
-
-  
-
+  groceries.each do |type, items|
+    items.each do |attribute, value|
+      array.push(attribute)
+    end
+  end
+  array
 end
